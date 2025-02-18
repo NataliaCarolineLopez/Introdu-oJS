2e3ca593a0d16119a99c0709a764a0dfbfb1adbe
NataliaCarolineLopez <nataliacarolinesenai@gmail.com>
2025-02-18 11:16:48 -0300

    Update script.js

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,3 +1,17 @@
-const clicou = () => {
-    console.log ("Você clicou no botão")
+// // const clicou = () => {
+//     console.log ("Você clicou no botão")
+// }
+
+let lista= document. querySelectorAll('.item')
+console.log(lista)
+
+let next= document. getElementById('.proximo')
+let prev= document. getElementById('.anterior')
+
+let contar = lista.length
+let ativo = 0
+console.log(contar) 
+
+next.onclick = () =>{
+    console.log ("proximo")
 }
